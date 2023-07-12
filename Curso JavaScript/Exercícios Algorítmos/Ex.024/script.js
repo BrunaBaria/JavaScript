9cba139fe2d515860c23b3687cb35cc5ba439e02
BrunaBaria <bruna.baria.m@gmail.com>
2023-07-12 18:07:43 -0300

    commit ex.24 algoritmos
    resolução do exercício 24 algoritmo

---
+++ Curso JavaScript/Exercícios Algorítmos/Ex.024/script.js
@@ -0,0 +1,20 @@
+function calcular(){
+    var km = document.getElementById('txtkm')
+    var res = document.querySelector('div#res')
+    var img = document.createElement('img')
+    var distancia = Number(km.value)
+    
+    if(distancia <= 200){
+        var passagem = distancia*0.50
+        img.setAttribute('src','carro200km.jpg')
+        res.innerHTML = `<p>O valor da passagem para ${distancia} km percorridos será ${passagem} reais</p>`
+        document.body.style.backgroung = '#c09da7'
+    }else{
+        var passagem = distancia*0.45
+        img.setAttribute('src','carromaiskm.jpg')
+        res.innerHTML=`<p>O valor da passagem para ${distancia}km percorridos será ${passagem} reais</p>`
+        document.body.style.backgroung = '#a897a7'
+    }
+    res.appendChild(img)
+    res.style.textAlign = 'center'
+}
