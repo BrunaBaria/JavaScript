d12b16fd4b8572cab0d4da1a2648c2c0c55fb6b0
BrunaBaria <bruna.baria.m@gmail.com>
2023-07-13 10:00:22 -0300

    commit ex.24 algoritmos
    melhorei o CSS do ex.24 algoritmo

--- Curso JavaScript/Exercícios Algorítmos/Ex.024/script.js
+++ Curso JavaScript/Exercícios Algorítmos/Ex.024/script.js
@@ -7,12 +7,12 @@ function calcular(){
     if(distancia <= 200){
         var passagem = distancia*0.50
         img.setAttribute('src','carro200km.jpg')
-        res.innerHTML = `<p>O valor da passagem para ${distancia} km percorridos será ${passagem} reais</p>`
+        res.innerHTML = `<p>O valor da passagem para ${distancia} km percorridos será R$ ${passagem.toFixed(2).replace('.',',')}</p>`
         document.body.style.backgroung = '#c09da7'
     }else{
         var passagem = distancia*0.45
         img.setAttribute('src','carromaiskm.jpg')
-        res.innerHTML=`<p>O valor da passagem para ${distancia}km percorridos será ${passagem} reais</p>`
+        res.innerHTML=`<p>O valor da passagem para ${distancia}km percorridos será R$ ${passagem.toFixed(2).replace('.',',')}</p>`
         document.body.style.backgroung = '#a897a7'
     }
     res.appendChild(img)
