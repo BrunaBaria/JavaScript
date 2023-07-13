1ab12e12d7c223e373533de45358d526d7638c3b
BrunaBaria <bruna.baria.m@gmail.com>
2023-07-13 11:59:53 -0300

    commit aula 14
    exercício da aula 14

---
+++ Curso JavaScript/Aula 14 - Repetições (parte 2)/ambiente.js
@@ -0,0 +1,26 @@
+//Repetição com variável de controle
+//funciona para quandoeu souber o limite das minhas execuções
+
+console.log('Vai começar...')
+for(var c =1;c<=5;c++){
+    console.log(c)
+}
+console.log('FIM!')
+
+/*
+Repetição com teste lógico no final
+var c = 1
+do{
+    console.log(c)
+    c++
+}while(c<=5)
+/*
+
+/*
+Repetição com teste lógico no início
+var c = 1
+while (c <=5){
+    console.log(c)
+    c++
+}
+*/
