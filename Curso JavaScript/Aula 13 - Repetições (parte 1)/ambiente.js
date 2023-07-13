0c510e78736df66a2afed5e735b16c5d940a566a
BrunaBaria <bruna.baria.m@gmail.com>
2023-07-13 10:49:12 -0300

    commit aula 13
    Organizei minhas pastas de exercícios e anotações. Adicionei  anotações da aula 13 - repetições do/while, while

---
+++ Curso JavaScript/Aula 13 - Repetições (parte 1)/ambiente.js
@@ -0,0 +1,26 @@
+//Repetição com teste lógico no final:
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++ // c= c+ 1
+}while (c<=6)
+
+/*
+//Repetição com teste lógico no início:
+
+var c = 1
+while (c<=6){
+    console.log(`Passo ${c}`)
+    c++ // c= c+ 1
+*/
+
+/*
+//Pior escolha de código para estrutura de repetição:
+
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
