a06243a595c8b957f4d88831a4f79cbba77f6e32
BrunaBaria <bruna.baria.m@gmail.com>
2023-07-21 14:16:48 -0300

    ex.016 e 045

---
+++ Curso JavaScript/Aula 14 - Repetições (parte 2)/Aula 14 Exercícios/Ex.016/script.ex.016.js
@@ -0,0 +1,31 @@
+function contar(){
+    let res = document.getElementById('res')
+    let ini= document.getElementById('txti')
+    let fim=document.getElementById('txtf')
+    let passo=document.getElementById('txtp')
+
+    if(ini.value.length ==0 || fim.value.length==0 || passo.value.length==0){
+        res.innerHTML=`Impossível contar`
+        //window.alert('[ERRO] Faltam valores!')
+    }else{
+        res.innerHTML = 'Contando: </br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p<=0){
+            window.alert('Passo inválido! Considerando Passo = 1')
+            p=1
+       }
+        if(i<f){
+            //contagem crescente
+            for(let c=i; c<=f;c+=p){
+                res.innerHTML+=` ${c}\u{1F449}`	
+            }
+        }else{
+            //contagem descrescente
+            for(c=i; c>=f;c-=p){
+                res.innerHTML +=` ${c}\u{1F449}`	
+            }
+        }res.innerHTML +=`\u{1F3C1}`
+       }
+    }
