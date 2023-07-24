bec3cc7606d80d30e9d236f796c5bc4d60375a10
BrunaBaria <bruna.baria.m@gmail.com>
2023-07-24 17:30:40 -0300

    update ex.044

--- Curso JavaScript/Exercícios Algorítmos/Ex.044- Contagem com Incremento/script.ex.044.js
+++ Curso JavaScript/Exercícios Algorítmos/Ex.044- Contagem com Incremento/script.ex.044.js
@@ -1,14 +1,22 @@
 function contar(){
-    var c = 2
+    
     var txtn1 = document.getElementById('txtn1')
     var txtn2 = document.getElementById('txtn2')
+    var inc = document.getElementById('txtin')
 
     var n1= Number(txtn1.value)
     var n2= Number(txtn2.value)
+    var p = Number(txtin.value)
 
     var res = document.getElementById('res')
-
-    for(var n1;n1<=n2;n1=n1+c){
-        res.innerHTML +=` ${n1} `
+    res.innerHTML = ``
+    if (n1<n2){
+        for(var c =n1;c<=n2;c+=p){
+            res.innerHTML +=` ${c} `
+        }
+    }else{
+        for(c=n1;c>=n2;c-=p){
+            res.innerHTML += ` ${c} `
+        }
     }res.innerHTML += ` Acabou!`
 }   
