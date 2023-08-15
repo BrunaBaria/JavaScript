db6476b774d970b00855d00b1ff7a27c17d25efe
BrunaBaria <bruna.baria.m@gmail.com>
2023-08-15 18:56:09 -0300

    up

---
+++ Curso JavaScript/Exercícios Algorítmos/Ex.071 -/script.ex.071.js
@@ -0,0 +1,7 @@
+function verificar(){
+    let valores = [999,999,999,999,999,999,999,999]
+    for(let pos in valores){
+        console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    }
+}
+
