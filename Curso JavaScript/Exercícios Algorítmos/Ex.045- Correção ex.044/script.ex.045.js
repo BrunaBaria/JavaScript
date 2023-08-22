d0d15d1e0e89b0e9cf18d30a4a44cb62e0d622e1
BrunaBaria <bruna.baria.m@gmail.com>
2023-08-22 09:32:29 -0300

    update

--- Curso JavaScript/Exercícios Algorítmos/Ex.045- Correção ex.044/script.ex.045.js
+++ Curso JavaScript/Exercícios Algorítmos/Ex.045- Correção ex.044/script.ex.045.js
@@ -4,7 +4,7 @@ function contar(){
      let txtn2=document.querySelector('input#txtn2')
      let txtn3=document.querySelector('input#txtn3')
 
-     let c = Number.parseInt(txtn1.value)
+     let n1 = Number.parseInt(txtn1.value)
      let n2 = Number.parseInt(txtn2.value)
      let i = Number.parseInt(txtn3.value)
     
@@ -14,11 +14,11 @@ function contar(){
           i = 1
      }
      if(n1<n2){
-          for( c=n1;c<=n2;c+=i){
+          for(let c=n1;c<=n2;c+=i){
                 res.innerHTML +=` ${c} `
           }
      }else{
-        for(c=n1; c>=n2;c-=i)
+        for(let c=n1; c>=n2;c-=i)
         res.innerHTML += ` ${c} `
    }res.innerHTML += ` Acabou!` 
 }
