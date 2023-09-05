ce4572ad5fa78ad9b4b2e190203e28e46efdefcb
BrunaBaria <bruna.baria.m@gmail.com>
2023-09-05 19:51:32 -0300

    up

--- Curso JavaScript/Exercícios Algorítmos/Ex.051 - Maior e menor número na repetição/script.ex.051.js
+++ Curso JavaScript/Exercícios Algorítmos/Ex.051 - Maior e menor número na repetição/script.ex.051.js
@@ -1,16 +1,18 @@
-let numeros =[]
-function informar(){
+
+/*function informar(){
     let res = document.querySelector('div#res')
     let num = Number.parseInt(document.querySelector('input#txtnum').value)
+   
+    numeros = []
     let precoMaior = 0
     let precoMenor = 0
 
     res.innerHTML+=''
-    for(let cont= 0; cont<5;cont++){
-      
-        numeros[cont] 
+
+    for(let cont= 0; cont<8;cont++){
+        numeros[cont]=
         numeros.push(num) 
         
     }
     res.innerHTML+=(`Os valores informados foram:  ${numeros}`)
-}
+}*/
