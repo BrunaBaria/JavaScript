9994bd4a22d7f3da7a3eabfcbf6f5b9453d837fa
BrunaBaria <bruna.baria.m@gmail.com>
2023-09-06 17:41:01 -0300

    up

---
+++ Curso JavaScript/Exercícios Algorítmos/Ex.052 - Idades por parâmetros/script.ex.052.js
@@ -0,0 +1,19 @@
+import prompt from 'prompt-sync';
+let leia = prompt()
+
+function verificarIdades(){
+    var res = document.querySelector('div#res');
+    var idades = []
+    var media = ''
+    var maior18 = ''
+    var menor5= ''
+    var maiorIdade = ''
+
+    contador = 1
+
+    while (contador <= 10){
+        contador++
+        console.log('Informe uma Idade: ')
+        idades[contador] +='' 
+    }
+}
