8f7dedc42c3f552108580563284711f51641dbf5
BrunaBaria <bruna.baria.m@gmail.com>
2023-09-19 11:29:52 -0300

    up

--- Curso JavaScript/Aula 14 - Repetições (parte 2)/Aula 14 Exercícios/Ex.016 - Contador com Passos/script.ex.016.js
+++ Curso JavaScript/Aula 14 - Repetições (parte 2)/Aula 14 Exercícios/Ex.016 - Contador com Passos/script.ex.016.js
@@ -1,27 +1,27 @@
 function contar(){
     let res = document.getElementById('res')
-    let i= Number(document.getElementById('txti').value)
-    let f=Number(document.getElementById('txtf').value)
-    let p=Number(document.getElementById('txtp').value)
+    let i = Number(document.getElementById('txti').value)
+    let f = Number(document.getElementById('txtf').value)
+    let p = Number(document.getElementById('txtp').value)
 
-    if(i.length ==0 || f.length==0 || p.length==0){
-        res.innerHTML=`Impossível contar`
+    if ( i.length == 0 || f.length == 0 || p.length == 0 ){
+        res.innerHTML =`Impossível contar`
         //window.alert('[ERRO] Faltam valores!')
-    }else{
+    } else {
         res.innerHTML = 'Contando: </br>'
         res.innerHTML = ``
-        if(p<=0){
+        if ( p <= 0){
             window.alert('Passo inválido! Considerando Passo = 1')
-            p=1
+            p = 1 
        }
-        if(i<f){
+        if ( i < f ){
             //contagem crescente
-            for(let c=i; c<=f;c+=p){
-                res.innerHTML+=` ${c}\u{1F449}`	
+            for ( let c = i; c <= f; c += p){
+                res.innerHTML +=` ${c}\u{1F449}`	
             }
-        }else{
+        } else {
             //contagem descrescente
-            for(c=i; c>=f;c-=p){
+            for ( c = i; c >= f; c -= p) {
                 res.innerHTML +=` ${c}\u{1F449}`	
             }
         }res.innerHTML +=`\u{1F3C1}`
--- Curso JavaScript/Aula 14 - Repetições (parte 2)/Aula 14 Exercícios/Ex.017 - Tabuada/script.ex.017.js
+++ Curso JavaScript/Aula 14 - Repetições (parte 2)/Aula 14 Exercícios/Ex.017 - Tabuada/script.ex.017.js
@@ -1,19 +1,21 @@
 function calcular(){
-    let num = document.getElementById('txtn')
+    let n = Number(document.getElementById('txtn').value)
     let tab = document.getElementById('seltab')
 
-    if(num.value.length == 0){
+    if ( n.length == 0 ) {
         window.alert('Por favor, digite um número!')
-    }else{
-        let n = Number(num.value)
-        let c=0
-        tab.innerHTML = ``
-        while(c<=10){
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            tab.value = `tab${c}`
-            tab.appendChild(item)
-            c++
+    } else {
+        let c = 1
+        tab.innerHTML = `` //limpar a tabela
+        while ( c <= 10 ) {
+            /*
+            criei uma variável item para guardar os itens dentro do elemento criado e o parâmetro 'option' são as opções presentes dentro do elemento
+            */
+            let item = document.createElement('option') //criei um elemento dentro da minha tabela no JS
+            item.text = `${n} x ${c} = ${n*c}` //item.text diz que o item é uma string
+            item.value = `tab${c}`//cada item da tabela vai receber um valor
+            tab.appendChild(item) //para aparecer na tabela, eu chamo a tab e informo que o elemento filho dela vai ser o item
+            c++ //incremente o elemento no final
         }
     }
     
