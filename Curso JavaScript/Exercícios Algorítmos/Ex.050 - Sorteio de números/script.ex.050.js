71b810f8772a265a79a3bf50d23a5f522f053faa
BrunaBaria <bruna.baria.m@gmail.com>
2023-09-19 11:42:05 -0300

    up

--- Curso JavaScript/Exercícios Algorítmos/Ex.050 - Sorteio de números/script.ex.050.js
+++ Curso JavaScript/Exercícios Algorítmos/Ex.050 - Sorteio de números/script.ex.050.js
@@ -6,16 +6,16 @@ function sortear(){
     let divisivel3 =0 
    
    res.innerHTML =''
-    while(c<20){
-        let escolhaDoComputador = escolhasPossiveis[Math.floor(Math.random()*escolhasPossiveis.length)]
-        res.innerHTML +=` ${escolhaDoComputador} `
-        if(escolhaDoComputador >5){
+    while( c < 20 ) {
+        let escolhaDoComputador = escolhasPossiveis [ Math.floor( Math.random()*escolhasPossiveis.length)] //Armazenei na [ ] os números sorteados pela máquina. Para pegar valores aleatórios utilize Math.floor(Math.radon()*___) e no ___ você coloca o intervalo que deseja sortear, nesse caso eu coloquei o tamanho das escolhasPossiveis, mas poderia ser valores específicos como (max – min + 1)onde max é o valor máximo e min é o valor mínimo do intervalo...
+        res.innerHTML +=` ${escolhaDoComputador} ` //mostra os números sorteados
+        if ( escolhaDoComputador > 5){
             ++maior5
-        }else if(escolhaDoComputador % 3==0){
+        } else if ( escolhaDoComputador % 3 == 0) {
             ++divisivel3
         }
         ++c
-    }res.innerHTML +=`</br> Os números sorteados maiores que 5 são ${maior5}`
+    } res.innerHTML +=`</br> Os números sorteados maiores que 5 são ${maior5}`
     res.innerHTML+=`</br> Os números sorteados divisíveis por 3 são ${divisivel3}`
 }
     
